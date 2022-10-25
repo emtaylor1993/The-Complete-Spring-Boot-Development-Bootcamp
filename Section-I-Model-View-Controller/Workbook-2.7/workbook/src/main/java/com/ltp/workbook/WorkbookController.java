cbda080c2ac6b36939d9dcd9886a6ebefed35e71
Emmanuel M. Taylor Jr <93065901+emtaylor1993@users.noreply.github.com>
2022-10-25 14:19:56 -0400

    * Updated WorkbookController.java

--- Section-I-Model-View-Controller/Workbook-2.7/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
+++ Section-I-Model-View-Controller/Workbook-2.7/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
@@ -9,7 +9,7 @@ public class WorkbookController {
     
     @GetMapping("/")
     public String getName(Model model) {
-        model.addAttribute("name", "Rayan");
+        model.addAttribute("name", "Emmanuel");
         return "name-list";
     }
 }
