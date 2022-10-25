f4ca7489b00a50cdef2eee6ed60eb6d2f068d8c8
Emmanuel M. Taylor Jr <93065901+emtaylor1993@users.noreply.github.com>
2022-10-25 14:48:01 -0400

    * Updated UtilityMethodsApplication.java

--- Section-I-Model-View-Controller/utility-methods/src/main/java/com/ltp/utilitymethods/UtilityMethodsApplication.java
+++ Section-I-Model-View-Controller/utility-methods/src/main/java/com/ltp/utilitymethods/UtilityMethodsApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UtilityMethodsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UtilityMethodsApplication.class, args);
 	}
-
 }
