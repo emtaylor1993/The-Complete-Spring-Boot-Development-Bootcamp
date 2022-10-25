44d45d301acb3f63cdc87b89b35eee36be5125b4
Emmanuel M. Taylor Jr <93065901+emtaylor1993@users.noreply.github.com>
2022-10-25 14:30:36 -0400

    * Updated WorkbookController.java

--- Section-I-Model-View-Controller/Workbook-2.9/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
+++ Section-I-Model-View-Controller/Workbook-2.9/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 public class WorkbookController {
     @GetMapping(value="/")
@@ -16,6 +15,4 @@ public class WorkbookController {
     public String away() {
         return "away";
     }
-
-    
 }
