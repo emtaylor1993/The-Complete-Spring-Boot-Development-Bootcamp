918ea9a8de9bb96e272f41f0bb5e2d88da431aa6
Emmanuel M. Taylor Jr <93065901+emtaylor1993@users.noreply.github.com>
2022-10-25 14:33:50 -0400

    * Updated WorkbookController.java

--- Section-I-Model-View-Controller/Workbook-2.10/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
+++ Section-I-Model-View-Controller/Workbook-2.10/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
@@ -2,15 +2,12 @@ package com.ltp.workbook;
 
 import java.util.Arrays;
 import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class WorkbookController {
-
     List<Employee> employees = Arrays.asList(
         new Employee("Jim Halpert", 32, "Salesman"),
         new Employee("Andy Bernard", 38, "Salesman"),
@@ -20,12 +17,10 @@ public class WorkbookController {
         new Employee("Angela Martin", 35, "Accountant"),
         new Employee("Dwight Schrute", 37, "Assistant to the Regional Manager")
     );
-
     
     @GetMapping(value="/")
     public String getStaff(Model model) {
         model.addAttribute("employees", employees);
         return "staff";
     }
-    
 }
