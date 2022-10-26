91e8379c5769ea2b65eea7fe542d178119a08a37
emtaylor1993 <etaylorsubs1993@gmail.com>
2022-10-26 09:35:51 -0400

    + Added code for Workbook 3.3

---
+++ Section-II-Field-Validation/Workbook-3.3/workbook/src/main/java/com/ltp/workbook/WorkbookApplication.java
@@ -0,0 +1,11 @@
+package com.ltp.workbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkbookApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WorkbookApplication.class, args);
+	}
+}
