7d63c2b7fb07f8344e18a57ca0b5099d51bf28fb
emtaylor1993 <etaylorsubs1993@gmail.com>
2022-10-26 21:30:09 -0400

    + Added code for Workbook 5.1

---
+++ Section-IV-Beans-and-Dependency-Injection/Workbook-5.1/grade-submission/src/test/java/com/ltp/gradesubmission/GradeSubmissionApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ltp.gradesubmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeSubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
