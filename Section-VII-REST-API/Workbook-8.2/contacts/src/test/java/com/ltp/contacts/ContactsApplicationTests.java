c3a0e991bfe2e560cc9acc706eec085b3cd09fb9
emtaylor1993 <etaylorsubs1993@gmail.com>
2022-11-01 13:58:27 -0400

    + Added code for Workbook 8.2

---
+++ Section-VII-REST-API/Workbook-8.2/contacts/src/test/java/com/ltp/contacts/ContactsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ltp.contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
